2371c0f5dfa1927c334449c5622e06c437843fa8
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-01-07 23:39:53 +0530

    January 7th Leetcode Daily Question

---
+++ January/7/Code.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long 
+int numberOfArithmeticSlices(vector<int>& nums) {
+    int n = nums.size();
+    map<pair<int, long long>, int> dp;
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<i; j++){
+            ll diff = (ll)nums[i]-(ll)nums[j];
+            dp[{i, diff}]++;
+            if(dp.find({j, diff}) != dp.end()){
+                dp[{i, diff}] += dp[{j, diff}];
+                ans+=dp[{j, diff}];
+            }
+        }
+    }
+    return ans;
+}
