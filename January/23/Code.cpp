661c41b603bd744b8cb0a8f7656f130f37a63b39
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-01-23 23:54:54 +0530

    January 23rd 📅 Leetcode Daily Question️‍🔥

---
+++ January/23/Code.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    int dfs(int idx, vector<string>& arr, vector<int> vis) {
+        if (idx == arr.size()) return 0;
+        int skip = dfs(idx + 1, arr, vis);
+
+        // Check if characters in the current string are unique
+        for (char c : arr[idx]) {
+            if (vis[c - 'a']) return skip;
+            vis[c - 'a'] = 1;
+        }
+
+        int noskip = arr[idx].length() + dfs(idx + 1, arr, vis);
+
+        return max(skip, noskip);
+    }
+
+public:
+    int maxLength(vector<string>& arr) {
+        vector<int> vis(26, 0);
+        return dfs(0, arr, vis);
+    }
+};
