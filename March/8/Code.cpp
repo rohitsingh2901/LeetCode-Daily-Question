c97e3b2d410e3a96d41f3fbec4bc5879b02f7cd4
rohitsingh2901 <singhrohitrrr@gmail.com>
2024-03-08 23:30:13 +0530

    8th March 📅 Leetcode Daily Question️‍🔥

---
+++ March/8/Code.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int maxFrequencyElements(vector<int>& nums) {
+        unordered_map<int,int> m;
+        for(int i:nums){
+            m[i]++;
+        }
+        int ma=0,res=0;
+        for(auto i:m){
+            ma=max(ma,i.second);
+        }
+        for(auto i:m){
+            if(i.second==ma){
+                res+=i.second;
+            }
+        }
+        return res;
+    }
+};
